df36020ec5e94ac624306352b2f9393560c2bb7c
Immutability, LLC <immutability.org@gmail.com>
2018-01-14 13:01:04 -0500

    Tidying up for presentation

--- scripts/mfa.sh
+++ scripts/mfa.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
+function print_help {
+    echo "Usage: bash mfa.sh ARGUMENTS"
+    echo -e "\nARGUMENTS:"
+    echo -e "  [keybase user]"
+    echo -e "  [Duo API Hostname]"
+    echo -e "  [Duo Integration Key]"
+    echo -e "  [Duo Secret Key]"
+}
 
-export VAULT_TOKEN=$(keybase decrypt -i $3_VAULT_TOKEN.txt)
+if [ -z "$4" ]; then
+    print_help
+    exit 0
+else
+    DUO_API_HOSTNAME=$2
+    DUO_INTEGRATION_KEY=$3
+    DUO_SECRET_KEY=$4
+fi
+
+export VAULT_TOKEN=$(keybase decrypt -i $1_VAULT_TOKEN.txt)
 
 vault write auth/github/mfa_config type=duo
 vault write auth/github/duo/access \
